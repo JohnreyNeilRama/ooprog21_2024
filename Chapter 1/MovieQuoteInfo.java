ae77e74ad280c197d73086e774f623cb22164be8
Johnrey456 <johnreyneil.rama@gmail.com>
2024-08-14 23:32:23 +0800

    Create MovieQuoteInfo.java

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,17 @@
+public class MovieQuoteInfo
+{
+  public static void main(String[] args)
+  {
+    String quote = "Women, they have minds and they have souls as well as just hearts.";
+    String movie = "Little Women";
+    String character = "Greta Gerwigs";
+    int year = 2019;
+    
+    System.out.println("Quote: " + quote);
+    System.out.println("Movie: " + movie);
+    System.out.println("Character: " + character);
+    System.out.println("Year: " + year);
+        
+    
+  }
+}
