f3a4c7e1d7d9b4e4c34eba408cbc77d28d2e1464
Johnrey456 <johnreyneil.rama@gmail.com>
2024-08-14 23:34:32 +0800

    Create TableAndChairs.java

---
+++ Chapter 1/TableAndChairs.java
@@ -0,0 +1,16 @@
+public class TableAndChairs
+{
+  public static void main (String[] args)
+  {
+     System.out.println("X                                 X");
+     System.out.println("X                                 X");
+     System.out.println("X        XXXXXXXXXXXXXXXX         X"); 
+     System.out.println("XXXXXXX  X              X  XXXXXXXX");
+     System.out.println("X     X  X              X  X      X"); 
+     System.out.println("X     X  X              X  X      X"); 
+
+
+
+                                 
+  }
+}
