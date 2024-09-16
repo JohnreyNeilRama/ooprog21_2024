1b22b8a06184648ffc6ff96fb48c79f3028f62cd
Johnrey Neil Rama <johnreyneil.rama@gmail.com>
2024-09-16 21:42:11 +0800

    Create FirstAddressExercise.java

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,15 @@
+public class FirstAddressExercise {
+
+    public static void main(String[] args) {
+        System.out.println("First Java application");
+        displayAddress();
+    }
+
+    public static void displayAddress() {
+        System.out.println("OPPO Company");
+        System.out.println("Building 15, No. 18, Xinghui Road, Science Park");
+        System.out.println("Nanshan District, Shenzhen, Guangdong");
+        System.out.println("China");
+        System.out.println("Company: OPPO Corporation");
+    }
+}
